4469653201b6a93ca50cfc2ecf96b0ab708aa8df
Maya Raskin <mayaraskin14@gmail.com>
2024-05-23 20:42:02 +0300

    Added STUDENT_ID to .env

--- environments/index.ts
+++ environments/index.ts
@@ -4,3 +4,4 @@ configDotenv();
 
 export const SERVER_HOST = process.env.SERVER_HOST as string;
 export const SERVER_PORT = parseInt(process.env.SERVER_PORT);
+export const STUDENT_ID = parseInt(process.env.STUDENT_ID);
