51f33126d86f15b702c7667fa190b2c41adb9a86
Maya Raskin <mayaraskin14@gmail.com>
2024-05-23 20:55:53 +0300

    Implemented makeAllRequests

--- modules/index.ts
+++ modules/index.ts
@@ -1,6 +1,28 @@
+import {IResponse} from "./types";
+import handlers from "./handlers";
+import {STUDENT_BIRTH_YEAR, STUDENT_ID} from "../environments";
 
-const makeAllRequests = () => {
-    
+const makeAllRequests = async () => {
+    const getResponse: string = await handlers.getHandler({
+        id: STUDENT_ID,
+        year: STUDENT_BIRTH_YEAR,
+    })
+
+    const postResponse: IResponse = await handlers.postHandler({
+        id: STUDENT_ID,
+        year: STUDENT_BIRTH_YEAR,
+        requestId: getResponse,
+    })
+
+    const putResponse: IResponse = await handlers.putHandler({
+        id: STUDENT_ID,
+        year: STUDENT_BIRTH_YEAR,
+        requestId: postResponse.message,
+    })
+
+    await handlers.deleteHandler({
+        id: putResponse.message,
+    })
 }
 
 export default makeAllRequests
