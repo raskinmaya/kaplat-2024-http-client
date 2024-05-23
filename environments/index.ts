08e5a61cb38d6d08ee0c6984241013614368beb4
Maya Raskin <mayaraskin14@gmail.com>
2024-05-23 20:52:57 +0300

    Added student birth year to .env

--- environments/index.ts
+++ environments/index.ts
@@ -5,3 +5,4 @@ configDotenv();
 export const SERVER_HOST = process.env.SERVER_HOST as string;
 export const SERVER_PORT = parseInt(process.env.SERVER_PORT);
 export const STUDENT_ID = parseInt(process.env.STUDENT_ID);
+export const STUDENT_BIRTH_YEAR = parseInt(process.env.STUDENT_BIRTH_YEAR);
